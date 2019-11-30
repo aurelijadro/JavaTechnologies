4433a13e55765fdc13211a80ccc075d18971881e
aurelija <a.drobelyte@gmail.com>
2019-11-30 16:04:42 +0200

    moved all products to be a state of productListComponent

--- src/App.js
+++ src/App.js
@@ -4,71 +4,15 @@ import SumPricesComponent from './components/sumPricesComponent/sumPricesCompone
 import Product from './product/product';
 import './App.css';
 
-const productList = [
-  {
-    name: "Kitty",
-    id: 10111,
-    price: 99.99,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 1
-  },
-  {
-    name: "Mau",
-    id: 2402,
-    price: 150.55,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 15
-  },
-  {
-    name: "Shadow",
-    id: 66666666,
-    price: 450,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 5
-  },
-  {
-    name: "Whiskers",
-    id: 933512,
-    price: 113,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 3
-  },
-  {
-    name: "Panda",
-    id: 61166,
-    price: 220,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 3
-  },
-  {
-    name: "Dragon",
-    id: 103003,
-    price: 345,
-    description: "Fluufy kitty! mau mau mau mau mau",
-    quantity: 2
-  }
-];
-const newProducts = productList.map((item) => {
-  return new Product(item.name, item.id, item.price, item.description, item.quantity);
-});
 
-const set = new Set(newProducts);
-const map = new Map(newProducts.map(item => [item.id, item]));
-const comeBackAround = Array.from(map.values()).reduce((text, item) => text + item.name + "(" + item.price + " Eur.), ", "");
 
 function App() {
 
-  const filteredProducts = productList.filter(product => product.price <= 300);
+
   return (
     <div className="container mx-auto">
 
-      <ProductListComponent products={filteredProducts} />
-      <SumPricesComponent products={filteredProducts} />
-      {console.log(newProducts)}
-      {console.log(set)}
-      {console.log(map)}
-      {console.log(comeBackAround)}
-
+      <ProductListComponent />
     </div>
   );
 }
--- src/components/productListComponent/productListComponent.jsx
+++ src/components/productListComponent/productListComponent.jsx
@@ -1,14 +1,66 @@
 import React from 'react';
 import ProductCardComponent from '../productCardComponent/productCardComponent';
+import SumPricesComponent from '../sumPricesComponent/sumPricesComponent';
 
 class ProductListComponent extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    name: "Kitty",
+                    id: 10111,
+                    price: 99.99,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 1
+                },
+                {
+                    name: "Mau",
+                    id: 2402,
+                    price: 150.55,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 15
+                },
+                {
+                    name: "Shadow",
+                    id: 66666666,
+                    price: 450,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 5
+                },
+                {
+                    name: "Whiskers",
+                    id: 933512,
+                    price: 113,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 3
+                },
+                {
+                    name: "Panda",
+                    id: 61166,
+                    price: 220,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 3
+                },
+                {
+                    name: "Dragon",
+                    id: 103003,
+                    price: 345,
+                    description: "Fluufy kitty! mau mau mau mau mau",
+                    quantity: 2
+                }
+            ]
+        }
+    }
     render() {
-        var products = this.props.products.map(product => {
+        var products = this.state.products.map(product => {
             return (<div className="col-3 mx-auto my-3" key={product.id}>
                 <ProductCardComponent product={product} />
             </div>)
         });
-        return (<div className="row card-deck">{products}</div>);
+        return (<div className="row card-deck">{products}
+            <SumPricesComponent products={this.state.products} />
+        </div>);
     }
 
 }
